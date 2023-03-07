8f1dd17bdb03087bcd51deb12fb598ff9f298ef9
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2023-03-07 06:59:59 -0500

    corregido

--- tfc-app/src/app/app.module.ts
+++ tfc-app/src/app/app.module.ts
@@ -10,14 +10,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { ServisLoginResgisService } from './service/servisLoginResgis.service';
 import { HeadersComponent } from './headers/headers.component';
 import { ProductosComponent } from './productos/productos.component';
-<<<<<<< HEAD
 import { CategoriaComponent } from './categoria/categoria/categoria.component';
 import { FormsModule } from '@angular/forms';
-=======
 import { VerproductoComponent } from './verproducto/verproducto.component';
 //temporal eliminar ALERTAS
 
->>>>>>> a0785de1be08386ee04915f4ede03668a9215105
 
 @NgModule({
   declarations: [
@@ -28,11 +25,8 @@ import { VerproductoComponent } from './verproducto/verproducto.component';
     FooterComponent,
     HeadersComponent,
     ProductosComponent,
-<<<<<<< HEAD
-    CategoriaComponent
-=======
+    CategoriaComponent,
     VerproductoComponent
->>>>>>> a0785de1be08386ee04915f4ede03668a9215105
   ],
   imports: [
     BrowserModule,
