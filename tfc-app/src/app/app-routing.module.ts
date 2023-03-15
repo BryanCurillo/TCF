ec10d79fd6e1d9729e70916281902de4b987385f
Bryan Curillo <bryan.curillo.est@tecazuay.edu.ec>
2023-03-15 10:03:49 -0500

    adas

--- tfc-app/src/app/app-routing.module.ts
+++ tfc-app/src/app/app-routing.module.ts
@@ -17,6 +17,7 @@ import { ProductosComponent } from './productos/productos.component';
 import { RegistrarComponent } from './registrar/registrar.component';
 import { TruequeComponent } from './trueque/trueque.component';
 import { VerproductoComponent } from './verproducto/verproducto.component';
+import { ProductoOfertaComponent } from './producto-oferta/producto-oferta.component';
 
 
 
@@ -33,6 +34,7 @@ const routes: Routes = [/*{ path: '', redirectTo: '/adHeader', pathMatch: 'full'
 { path: 'admclientes', component: ListaClientesComponent },
 { path: 'trueque', component: TruequeComponent },
 { path: 'misproductos', component: MisproductosComponent },
+{ path: 'productOferta', component: ProductoOfertaComponent },
 { path: 'principalt', component: PrincipaltruequeComponent },
 
 //rutas para el administrador
